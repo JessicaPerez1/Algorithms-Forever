1a673b240451f3fb5994f5cce4c9557f5afc934b
Jessica Perez <1jessicaperez@gmail.com>
2020-10-07 13:36:46 -0400

    Is Palindrome solved

---
+++ is-palindrome/is-palindrome.js
@@ -0,0 +1,12 @@
+var isPalindrome = function (str) {
+  //define the reversed string
+  //split to seperate each char and store to an array
+  //reverse to change the order or chars
+  //join to reunit all array chars into a string
+  var reversedStr = str.split("").reverse().join("");
+  if (reversedStr === str) {
+    return true;
+  } else {
+    return false;
+  }
+};

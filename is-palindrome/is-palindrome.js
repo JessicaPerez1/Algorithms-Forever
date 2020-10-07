e410c388e144da1776fdba20007bec1b0553165e
Jessica Perez <1jessicaperez@gmail.com>
2020-10-07 14:20:44 -0400

    Acronym builder solved

--- is-palindrome/is-palindrome.js
+++ is-palindrome/is-palindrome.js
@@ -2,7 +2,7 @@ var isPalindrome = function (str) {
   //define the reversed string
   //split to seperate each char and store to an array
   //reverse to change the order or chars
-  //join to reunit all array chars into a string
+  //join to reunite all array chars into a string
   var reversedStr = str.split("").reverse().join("");
   if (reversedStr === str) {
     return true;
